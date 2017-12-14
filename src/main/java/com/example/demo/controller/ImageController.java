3f52c304e0bbbae9eb9f070413c883e371888ded
JaxYoun <gao88jie@qq.com>
2017-12-14 16:14:22 +0800

    图片转gif方法，最终解决方案：要求原图片为png，可以避免体积膨胀问题！

--- src/main/java/com/example/demo/controller/ImageController.java
+++ src/main/java/com/example/demo/controller/ImageController.java
@@ -5,6 +5,7 @@ import com.example.demo.util.gifUtil.AnimatedGifEncoder;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,22 +28,29 @@ public class ImageController {
 
     private static final ObjectMapper jacksonMapper = new ObjectMapper();
 
+    @Value("${gif.quality}")
+    private int gifQuality;
+
     @Autowired
     private AnimatedGifEncoder animatedGifEncoder;
 
     /**
-     * 将多帧图片编码为Gif
+     * 将多帧图片编码为Gif，原图最好是png，因为jpg会导致生成的gif体积过于膨胀
      *
      * @param json
      * @return
      */
     @PostMapping("/transImageToGif")
     public Object transImageToGif(@RequestBody String json) {
-
+        Map<String, Object> resultMap = new HashMap<>();
+        log.info("transImageToGif");
         String arg = null;
         try {
             arg = URLDecoder.decode(json, "UTF-8");
         } catch (UnsupportedEncodingException e1) {
+            resultMap.put("code", "500");
+            resultMap.put("message", "请求参数格式非法！");
+            resultMap.put("data", null);
             e1.printStackTrace();
         }
 
@@ -50,13 +58,14 @@ public class ImageController {
         try {
             framePath = jacksonMapper.readValue(arg, FramePath.class);
         } catch (IOException e) {
+            resultMap.put("code", "500");
+            resultMap.put("message", "请求参数格式非法！");
+            resultMap.put("data", null);
             e.printStackTrace();
         }
 
-        Map<String, Object> resultMap = new HashMap<>();
         List<String> resultList = encodeFrameListToGif(framePath.getPathList());
-
-        if(resultList == null) {
+        if (resultList == null) {
             resultMap.put("code", "500");
             resultMap.put("message", "请求参数非法！");
             resultMap.put("data", null);
@@ -70,29 +79,33 @@ public class ImageController {
 
     /**
      * 将多帧图片编码为Gif
+     *
      * @param framePathList
      * @return
      */
-    public static List<String> encodeFrameListToGif(List<String> framePathList) {
-        if(framePathList == null || framePathList.size() == 0) {
+    public List<String> encodeFrameListToGif(List<String> framePathList) {
+        if (framePathList == null || framePathList.size() == 0) {
             return null;
         }
 
         List<String> targetGifPathList = new ArrayList<>();
         AnimatedGifEncoder animatedGifEncoder = new AnimatedGifEncoder();
-        animatedGifEncoder.setDelay(10000);
+        animatedGifEncoder.setDelay(1);
         animatedGifEncoder.setRepeat(-1);
-        animatedGifEncoder.setQuality(50);
+        animatedGifEncoder.setQuality(gifQuality);
+//        animatedGifEncoder.setQuality(50);
 
         for (String framePath : framePathList) {
             int lastIndexOfDot = framePath.lastIndexOf('.');
             String targetGifPath = framePath.substring(0, lastIndexOfDot) + ".gif";
             animatedGifEncoder.start(targetGifPath);
-            InputStream inputStream = null;
+            InputStream inputStream_1 = null;
+            InputStream inputStream_2 = null;
             try {
-                inputStream = new FileInputStream(framePath);
-                animatedGifEncoder.addFrame(ImageIO.read(inputStream));
-                animatedGifEncoder.addFrame(ImageIO.read(inputStream));
+                inputStream_1 = new FileInputStream(framePath);
+                inputStream_2 = new FileInputStream(framePath); //"E:\\TEST\\ChengJie\\imageToGif\\test\\422-0-副本.jpg");
+                animatedGifEncoder.addFrame(ImageIO.read(inputStream_1));
+                animatedGifEncoder.addFrame(ImageIO.read(inputStream_2));
                 animatedGifEncoder.finish();
                 targetGifPathList.add(targetGifPath);
             } catch (FileNotFoundException e) {
@@ -100,21 +113,59 @@ public class ImageController {
             } catch (IOException e) {
                 log.error(e.getMessage(), e);
             } finally {
-                if (inputStream != null) {
+                if (inputStream_1 != null) {
                     try {
-                        inputStream.close();
+                        inputStream_1.close();
+                    } catch (IOException e) {
+                        log.error(e.getMessage(), e);
+                    }
+                }
+                if (inputStream_2 != null) {
+                    try {
+                        inputStream_2.close();
                     } catch (IOException e) {
                         log.error(e.getMessage(), e);
                     }
                 }
             }
         }
+
+        List<String> tempList = new ArrayList<>(targetGifPathList.size());
+        tempList.addAll(targetGifPathList);
+
+        while (tempList.size() != 0) {
+            Iterator<String> it = tempList.iterator();
+            if (it.hasNext() && ifFileExist(it.next())) {
+                it.remove();
+            }
+        }
+
         return targetGifPathList;
     }
 
+    /**
+     * 判断FileSystem上是否存在指定文件
+     *
+     * @param filePath
+     * @return
+     */
+    public static boolean ifFileExist(String filePath) {
+        boolean isExist;
+        File file = new File(filePath);
+        isExist = file.exists();
+        return isExist;
+    }
+
     public static void main(String[] args) {
-        String path = "E:\\TEST\\ChengJie\\imageToGif\\test\\blue.jpg";
-        int lastIndexOfDot = path.lastIndexOf('.');
-        System.out.println(path.substring(0, lastIndexOfDot));
+        /*String path = "E:\\TEST\\ChengJie\\imageToGif\\test\\png\\455-1.png";
+        List<String> list = new ArrayList<>();
+        list.add(path);
+        encodeFrameListToGif(list);*/
+
+       /*try {
+            Thumbnails.of("E:\\TEST\\ChengJie\\imageToGif\\test\\500179127.png").scale(1F).outputQuality(1F).toFile("E:\\TEST\\ChengJie\\imageToGif\\test\\500179127kkk.jpg");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
     }
 }
